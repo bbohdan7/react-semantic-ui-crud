69e46d482a5a83359c380bd5d79342955485804f
Bogdan <zbohdan7@gmail.com>
2021-10-24 14:05:18 +0300

    Added confirmation button with pretty locking when users's data hasn't been filled out

--- src/components/add-user.jsx
+++ src/components/add-user.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from 'react'
-import { Link, useHistory } from "react-router-dom";
-import { Grid, Form, Button, Icon } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
+import { Card, Segment, Grid, Form, Label, Button, Modal, Icon } from "semantic-ui-react";
 import 'semantic-ui-css/semantic.min.css'
 import UserService from "../services/userservice";
 
@@ -12,18 +12,34 @@ const AddUser = (props) => {
     let [firstname, setFirstname] = useState("")
     let [lastname, setLastname] = useState("")
     let [email, setEmail] = useState("")
+    let [showConfirm, setShowConfirm] = useState(false)
+    let [allFilled, setAllFilled] = useState(false)
 
     const onChangeFirstname = (e) => {
         setFirstname(e.target.value)
+        checkAllFilled()
     }
 
     const onChangeLastname = (e) => {
         setLastname(e.target.value)
+        checkAllFilled()
     }
 
     const onChangeEmail = (e) => {
         setEmail(e.target.value)
-        console.log(`${firstname} ${lastname} ${email}`)
+        checkAllFilled()
+    }
+
+    const showCreateConfirm = () => setShowConfirm(true)
+
+    const hideCreateConfirm = () => setShowConfirm(false)
+
+    const checkAllFilled = () => {
+        if (firstname && lastname && email) {
+            setAllFilled(true)
+        } else {
+            setAllFilled(false)
+        }
     }
 
     const createUser = () => {
@@ -42,9 +58,8 @@ const AddUser = (props) => {
                 <Button color="red" onClick={() => history.goBack()}><Icon name="arrow left" />Back</Button>
             </Grid.Column>
             <Grid.Column>
-                <h1>Adding new user</h1>
-
                 <Form>
+                    <h1 style={{ textAlign: "center" }}>Adding new user</h1>
                     <Form.Field>
                         <label>First name</label>
                         <input type="text" placeholder="Enter your first name" value={firstname} onChange={onChangeFirstname} />
@@ -57,10 +72,44 @@ const AddUser = (props) => {
                         <label>Email</label>
                         <input type="email" placeholder="Enter your email" value={email} onChange={onChangeEmail} />
                     </Form.Field>
-                    <Button color="green" onClick={createUser}><Icon name="pencil" />Create</Button>
                 </Form>
             </Grid.Column>
-            <Grid.Column></Grid.Column>
+            <Grid.Column>
+                <Card>
+                    <Card.Content>
+                        <Card.Header>Preview</Card.Header>
+                        <Card.Meta>User</Card.Meta>
+                        <Card.Description>
+                            <ul className="ui list">
+                                <li>First Name: {firstname}</li>
+                                <li>Last Name: {lastname}</li>
+                                <li>Email: {email}</li>
+                                <Segment>
+                                    {!allFilled ?
+                                        (<Label color="red" ribbon ><Icon name="lock" />Locked</Label>)
+                                        : (<Label color="green" ribbon ><Icon name="unlock" />Unlocked</Label>)
+                                    }
+                                    <Button color="green" onClick={showCreateConfirm} disabled={!allFilled}><Icon name="pencil" />Create</Button>
+                                </Segment>
+                            </ul>
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+                <Modal onClose={hideCreateConfirm} onOpen={showCreateConfirm} open={showConfirm} closeIcon size="mini">
+                    <Modal.Header>Are you sure to create user?</Modal.Header>
+                    <Modal.Content>
+                        <ul className="ui list">
+                            <li>First Name: {firstname}</li>
+                            <li>Last Name: {lastname}</li>
+                            <li>Email: {email}</li>
+                        </ul>
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color="red" onClick={hideCreateConfirm}><Icon name="close" />No</Button>
+                        <Button color="green" onClick={createUser}><Icon name="check" />Yes</Button>
+                    </Modal.Actions>
+                </Modal>
+            </Grid.Column>
         </Grid>
     )
 }
--- src/components/users-list.jsx
+++ src/components/users-list.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import UserService from '../services/userservice'
-import { Button, Checkbox, Grid, Divider, Header, Icon, Label, Table, Modal } from 'semantic-ui-react'
+import { Button, Checkbox, Grid, Header, Icon, Label, Table, Modal } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import { Link } from 'react-router-dom'
 
